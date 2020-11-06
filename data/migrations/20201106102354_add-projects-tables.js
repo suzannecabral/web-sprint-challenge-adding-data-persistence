1d1a5b175827432503acde15593ebd7d74b99170
Suzanne Cabral <suzannecabral@live.com>
2020-11-06 10:31:39 -0800

    Initialized db, added projects table thru migration

--- data/migrations/20201106102354_add-projects-tables.js
+++ data/migrations/20201106102354_add-projects-tables.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('projects', tbl=>{
+    tbl.increments();
+
+    tbl.text('name')
+      .notNullable()
+      .unique();
+
+    tbl.text('description');
+
+    tbl.boolean('completed')
+      .notNullable()
+      .defaultTo(false);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('projects');
 };
