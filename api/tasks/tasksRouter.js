74042373dc07de78947fc28df2fcd470e88cac3f
Suzanne Cabral <suzannecabral@live.com>
2020-11-06 18:48:11 -0800

    Added endpoints to get and post resources by task id

--- api/tasks/tasksRouter.js
+++ api/tasks/tasksRouter.js
@@ -24,4 +24,47 @@ router.get('/', (req,res)=>{
     });
 });
 
+
+  // get list of resources for task
+  // getResources(task_id) 
+  // GET /:id/resources
+  //-----------------------
+
+  router.get('/:id/resources', (req,res)=>{
+    const task_id = req.params.id;
+
+    Tasks.getResources(task_id)
+      .then(data =>{ 
+        res.status(200).json(data);
+      })
+      .catch(err=>{
+        console.log(err);
+        res.status(500).json({ message: 'Failed to get resources'});
+      });
+  });
+
+
+  // post a new resource for a task
+  // addResource(task_id, resource_id)
+  // POST /:id/resources
+  //-----------------------
+
+  router.post('/:id/resources', (req,res)=>{
+    const task_id = req.params.id;
+    const resource_id = req.body.resource_id;
+
+    // requires input: resource_id
+
+    Tasks.addResource(task_id, resource_id)
+      .then(data =>{ 
+        res.status(201).json(data);
+      })
+      .catch(err=>{
+        console.log(err);
+        res.status(500).json({ message: 'Failed to add resource'});
+      });
+  });
+
+
+
 module.exports = router;
